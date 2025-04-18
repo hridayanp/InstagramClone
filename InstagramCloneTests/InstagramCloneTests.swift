1138e3b228bea223207b12094f3d4d89158c29f4
hridayanp <hridayanphukan123@gmail.com>
2025-04-18 10:11:39 +0530

    Initial Commit

---
+++ InstagramCloneTests/InstagramCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneTests.swift
+//  InstagramCloneTests
+//
+//  Created by Hridayan Phukan on 18/04/25.
+//
+
+import Testing
+@testable import InstagramClone
+
+struct InstagramCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
