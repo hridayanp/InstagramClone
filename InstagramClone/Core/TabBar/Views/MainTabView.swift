9fa0ecf28e94fc44657d98b967b2f0fbaa42385b
hridayanp <hridayanphukan123@gmail.com>
2025-04-18 10:17:02 +0530

    initial project setup

---
+++ InstagramClone/Core/TabBar/Views/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Hridayan Phukan on 18/04/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
